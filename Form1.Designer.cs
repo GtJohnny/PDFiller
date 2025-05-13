808a73ba6552d113da658037e12782ceec7450ca
GtJohnny <ionutxd04@gmail.com>
2025-05-13 21:59:12 +0300

    modif

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -62,7 +62,6 @@
             this.emagBtn = new System.Windows.Forms.Button();
             this.SamedayBtn = new System.Windows.Forms.Button();
             this.CelBtn = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.groupBox8 = new System.Windows.Forms.GroupBox();
             this.rootTextBox = new System.Windows.Forms.TextBox();
             this.excelPathBox = new System.Windows.Forms.TextBox();
@@ -522,20 +521,6 @@
             this.CelBtn.UseVisualStyleBackColor = false;
             this.CelBtn.Click += new System.EventHandler(this.CelBtn_Click);
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.button2.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.Control;
-            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(448, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 39);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Load Images ";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Visible = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
-            // 
             // groupBox8
             // 
             this.groupBox8.BackColor = System.Drawing.SystemColors.Window;
@@ -715,7 +700,6 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 26);
             this.tabPage1.Name = "tabPage1";
@@ -868,7 +852,6 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel2;
         internal System.Windows.Forms.ComboBox drawComboBox;
         private System.Windows.Forms.Button button3;
